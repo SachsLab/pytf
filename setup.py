f2d63a2d97edcaa8b03a15b90970e6eca0763417
Chadwick Boulay <chadwick.boulay@gmail.com>
2020-08-08 01:48:10 -0400

    Cleanup for wheel

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import __future__
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 # Authors : David C.C. Lu <davidlu89@gmail.com>
@@ -7,15 +7,15 @@ import os
 # License : MIT
 
 setup(
-    name = "pytf",
-    version = 0.1,
-    packages = ['pytf', 'pytf.core', 'pytf.viz', 'pytf.viz.utils', 'pytf.filter', 'pytf.reconstruction', 'pytf.time_frequency', 'pytf.utilities'],
+    name="pytf",
+    version=0.1,
+    packages=find_packages(),
     install_requires=['numpy', 'scipy'],
-    author = "David Lu",
-    author_email = "davidlu89@gmail.com",
-    description = "pytf is a tool for time-frequency analysis",
+    author="David Lu",
+    author_email="davidlu89@gmail.com",
+    description="pytf is a tool for time-frequency analysis",
     url='https://github.com/davidlu89/pytf',
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
@@ -23,15 +23,11 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Operating System :: MacOS'],
-        platforms='any'
+    platforms='any'
 )
